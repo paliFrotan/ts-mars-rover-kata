3851b4dfe627e04f53c0ebd17760c0c2fc579684
Pali Frotan <pali.frotan@icloud.com>
2023-06-10 14:33:23 +0100

    Feat:M moves rover correctly

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,4 +1,6 @@
-import { createRover } from '../src/rover';
+import { createRover,
+         move,
+                 } from '../src/rover';
 
 describe('Rover', () => {
   it('should be initialized with a starting position and direction', () => {
@@ -6,5 +8,17 @@ describe('Rover', () => {
     expect(rover.x).toBe(0);
     expect(rover.y).toBe(0);
     expect(rover.direction).toBe('N');
+  });
+
+  it('should move forward in the direction it is facing', () => {
+    let rover = createRover(0, 0, 'N');
+    rover = move(rover, 'M');
+    expect(rover.x).toBe(0);
+    expect(rover.y).toBe(1);
+
+    rover = createRover(0, 0, 'E');
+    rover = move(rover, 'M');
+    expect(rover.x).toBe(1);
+    expect(rover.y).toBe(0);
   });
 });
--- src/rover.ts
+++ src/rover.ts
@@ -7,3 +7,14 @@ export interface Rover {
   export function createRover(x: number, y: number, direction: string): Rover {
     return { x, y, direction };
   }
+
+  export function move(rover: Rover, command: string): Rover {
+    if (command === 'M') {
+      if (rover.direction === 'N') {
+        return { ...rover, y: rover.y + 1 };
+      } else if (rover.direction === 'E') {
+        return { ...rover, x: rover.x + 1 };
+      }
+    }
+    return rover;
+  }
