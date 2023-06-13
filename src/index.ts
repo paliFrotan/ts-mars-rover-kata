1233d1eb93f3723d6f0ee7d4a221f23a969be6f0
Pali Frotan <pali.frotan@icloud.com>
2023-06-13 20:52:03 +0100

    Fix:tidy up

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import { add } from './src/calc';
-
-console.log(`2 plus 2 is ${add(2, 2)}`);
--- src/rover.ts
+++ src/rover.ts
@@ -9,17 +9,44 @@ export interface Plateau{
    depth: number;
 }
 
-export function createPlateau(width: number, depth: number) : Plateau{
+export interface ErrorMessage{
+  index: number;
+  userMessage: string;
+}
+
+export interface CollisionPoints{
+  posX: number;
+  posY: number;
+}
+
+export function createPlateau(sizeString: string) : Plateau | ErrorMessage {
+  if (sizeString.length !== 3 || isNaN(Number(sizeString[0])) || isNaN(Number(sizeString[2]))) {
+    return { index: 1, userMessage :'Invalid sizeString format. Expected format: "x y" where x and y are numbers.'};
+  }
+  let width = Number(sizeString[0]);
+  let depth = Number(sizeString[2]);
   width++;
   depth++;
   return {width, depth};
 }
 
-export function createRover(x: number, y: number, direction: string): Rover {
+export function createRover(roverString: string): Rover | ErrorMessage {
+  if (roverString.length !== 5 || isNaN(Number(roverString[0])) || isNaN(Number(roverString[2])) || !['N', 'E', 'S', 'W'].includes(roverString[4])) {
+    return { index : 2, userMessage:'Invalid roverString format. Expected format: "x y d" where x and y are numbers and d is one of "N", "E", "S", "W".'};
+  }
+
+  let x = Number(roverString[0]);
+  let y = Number(roverString[2]);
+  let direction = roverString[4];
+  
   return { x, y, direction };
 }
 
-export function move(rover: Rover, command: string, plateau: Plateau): Rover {
+export function areRoversColliding(rover1: Rover, rover2: Rover): boolean {
+  return rover1.x === rover2.x && rover1.y === rover2.y;
+}
+
+export function move(rover: Rover, command: string, plateau: Plateau): Rover | ErrorMessage {
   let newX = rover.x;
   let newY = rover.y;
   if (command === 'M') {
@@ -37,13 +64,15 @@ export function move(rover: Rover, command: string, plateau: Plateau): Rover {
     case 'W':
       newX = rover.x - 1;
       break;
-    }
+    };
     if (newX >= 0 && newX < plateau.width && newY >= 0 && newY < plateau.depth) {
+      
       return { ...rover, x: newX, y: newY };
+    } else {
+      return { index: 3, userMessage :`Rover@(${rover.x},${rover.y}) not moved since out of bounds of plateau.`};
     };
   }
   return rover;
-  
 } 
 
 
@@ -78,17 +107,31 @@ export function turnRight(rover: Rover, command: string): Rover {
   return rover;
 } 
 
-export function instructionsRover(rover: Rover, plateau: Plateau, instructions: string):string {
-  [...instructions].forEach((value: string) => {
-    switch(value){
-      case "L":
-        return rover = turnLeft(rover, value);
-      case "R":
-        return rover = turnRight(rover, value);
-      case "M":
-        return rover = move(rover, value, plateau);
+export function instructionsRover(rover: Rover, plateau: Plateau, instructions: string): string | ErrorMessage {
+  let result: Rover | ErrorMessage = rover;
+  for (const value of instructions) {
+    if ('userMessage' in result) {
+      // An error message has been returned, so we stop processing further instructions
+      break;
     }
-
-  });
-  return ""+rover.x+" "+rover.y+" "+rover.direction+"";
+    switch (value) {
+      case 'L':
+        result = turnLeft(result as Rover, value);
+        break;
+      case 'R':
+        result = turnRight(result as Rover, value);
+        break;
+      case 'M':
+        result = move(result as Rover, value, plateau);
+        break;
+    }
+  }
+  if ('index' in result && 'userMessage' in result) {
+    // An error message has been returned
+    return result;
+  } else {
+    // No error message has been returned
+    let collisionPoint  = {posX:result.x, posY:result.y};
+    return `${result.x} ${result.y} ${result.direction}`;
+  }
 }
