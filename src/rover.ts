606fd28fb35812c23319789b8c1dee8d7caf8e2d
Pali Frotan <pali.frotan@icloud.com>
2023-06-14 21:05:10 +0100

    point for pre-console

--- src/rover.ts
+++ src/rover.ts
@@ -1,79 +1,78 @@
 export interface Rover {
-    x: number;
-    y: number;
-    direction: string;
+  x: number;
+  y: number;
+  direction: string;
 }
 
-export interface Plateau{
-   width: number;
-   depth: number;
+export interface Plateau {
+  width: number;
+  depth: number;
 }
 
-export interface ErrorMessage{
+export interface ErrorMessage {
   index: number;
   userMessage: string;
 }
 
-export interface CollisionPoints{
+export interface CollisionPoints {
   posX: number;
   posY: number;
 }
 
-export function createPlateau(sizeString: string) : Plateau | ErrorMessage {
+export function createPlateau(sizeString: string): Plateau | ErrorMessage {
   if (sizeString.length !== 3 || isNaN(Number(sizeString[0])) || isNaN(Number(sizeString[2]))) {
-    return { index: 1, userMessage :'Invalid sizeString format. Expected format: "x y" where x and y are numbers.'};
+    return { index: 1, userMessage: 'Invalid sizeString format. Expected format: "x y" where x and y are numbers.' };
   }
   let width = Number(sizeString[0]);
   let depth = Number(sizeString[2]);
   width++;
   depth++;
-  return {width, depth};
+  return { width, depth };
 }
 
 export function createRover(roverString: string): Rover | ErrorMessage {
   if (roverString.length !== 5 || isNaN(Number(roverString[0])) || isNaN(Number(roverString[2])) || !['N', 'E', 'S', 'W'].includes(roverString[4])) {
-    return { index : 2, userMessage:'Invalid roverString format. Expected format: "x y d" where x and y are numbers and d is one of "N", "E", "S", "W".'};
+    return { index: 2, userMessage: 'Invalid roverString format. Expected format: "x y d" where x and y are numbers and d is one of "N", "E", "S", "W".' };
   }
 
   let x = Number(roverString[0]);
   let y = Number(roverString[2]);
   let direction = roverString[4];
-  
+
   return { x, y, direction };
 }
 
-export function areRoversColliding(rover1: Rover, rover2: Rover): boolean {
-  return rover1.x === rover2.x && rover1.y === rover2.y;
+export function isRoverCollidingWithPoints(rover: Rover, points: CollisionPoints[]): boolean {
+  return points.some(point => point.posX === rover.x && point.posY === rover.y);
 }
 
 export function move(rover: Rover, command: string, plateau: Plateau): Rover | ErrorMessage {
   let newX = rover.x;
   let newY = rover.y;
   if (command === 'M') {
-    
-    switch (rover.direction){
+
+    switch (rover.direction) {
       case 'N':
-      newY = rover.y + 1;
-      break;
-    case 'E':
-      newX = rover.x + 1;
-      break;
-    case 'S':
-      newY = rover.y - 1;
-      break;
-    case 'W':
-      newX = rover.x - 1;
-      break;
-    };
+        newY = rover.y + 1;
+        break;
+      case 'E':
+        newX = rover.x + 1;
+        break;
+      case 'S':
+        newY = rover.y - 1;
+        break;
+      case 'W':
+        newX = rover.x - 1;
+        break;
+    }
     if (newX >= 0 && newX < plateau.width && newY >= 0 && newY < plateau.depth) {
-      
       return { ...rover, x: newX, y: newY };
     } else {
-      return { index: 3, userMessage :`Rover@(${rover.x},${rover.y}) not moved since out of bounds of plateau.`};
-    };
-  }
+      return { index: 3, userMessage: `Rover@(${rover.x},${rover.y}) not moved since out of bounds of plateau.` };
+    }
+  } 
   return rover;
-} 
+}
 
 
 export function turnLeft(rover: Rover, command: string): Rover {
@@ -107,7 +106,7 @@ export function turnRight(rover: Rover, command: string): Rover {
   return rover;
 } 
 
-export function instructionsRover(rover: Rover, plateau: Plateau, instructions: string): string | ErrorMessage {
+export function instructionsRover(rover: Rover, plateau: Plateau, instructions: string, points: CollisionPoints[]): string | ErrorMessage {
   let result: Rover | ErrorMessage = rover;
   for (const value of instructions) {
     if ('userMessage' in result) {
@@ -123,6 +122,9 @@ export function instructionsRover(rover: Rover, plateau: Plateau, instructions: 
         break;
       case 'M':
         result = move(result as Rover, value, plateau);
+        if ('x' in result && isRoverCollidingWithPoints(result as Rover, points)) {
+          return { index: 4, userMessage: `Collision detected between rover@(${result.x},${result.y}) and collision point.` };
+        }
         break;
     }
   }
@@ -131,7 +133,7 @@ export function instructionsRover(rover: Rover, plateau: Plateau, instructions: 
     return result;
   } else {
     // No error message has been returned
-    let collisionPoint  = {posX:result.x, posY:result.y};
     return `${result.x} ${result.y} ${result.direction}`;
   }
 }
+
--- src/roverMars.test.ts
+++ src/roverMars.test.ts
@@ -4,6 +4,7 @@ import { createRover,
          turnLeft,
          turnRight,
          instructionsRover,
+         isRoverCollidingWithPoints,
                  } from './rover';
 
 
@@ -182,26 +183,30 @@ describe('Rover', () => {
 describe('Input to 2 rovers and their output', () => {
   it('test for plateau 6x6 and 2 rover moves', () => {
     let plateau = createPlateau("5 5");
+    
     if ('width' in plateau) {
       // plateau is of type Plateau
       expect(plateau.width).toBe(6);
       expect(plateau.depth).toBe(6);
       let rover1 = createRover('1 2 N');
       if ('x' in rover1){
-        let execute = instructionsRover(rover1, plateau, "LMLMLMLMM");
+        let points =[{posX:-1,posY:-1}];
+        let execute = instructionsRover(rover1, plateau, "LMLMLMLMM",points);
         expect(execute).toBe("1 3 N");
       } else {
         expect(rover1.userMessage).toBe('Invalid roverString format. Expected format: "x y d" where x and y are numbers and d is one of "N", "E", "S", "W".');
       }
       let rover2 = createRover('3 3 E');
+      let points =  [{ posX: 1, posY: 2 }];
       if ('x' in rover2){
-        expect(instructionsRover(rover2, plateau, "MMRMMRMRRM")).toBe("5 1 E");
+        expect(instructionsRover(rover2, plateau, "MMRMMRMRRM",points)).toBe("5 1 E");
       } else {
         expect(rover2.userMessage).toBe('Invalid roverString format. Expected format: "x y d" where x and y are numbers and d is one of "N", "E", "S", "W".');
       }
       let rover3 = createRover('3 4 E');
+      points.push({ posX: 5, posY: 1 });
       if ('x' in rover3){
-        let execute = instructionsRover(rover3, plateau, "MMMMMMMM");
+        let execute = instructionsRover(rover3, plateau, "MMMMMMMM",points);
         expect(execute).toEqual({index: 3,userMessage: 'Rover@(5,4) not moved since out of bounds of plateau.'});
       } else {
         expect(rover3.userMessage).toBe('Invalid roverString format. Expected format: "x y d" where x and y are numbers and d is one of "N", "E", "S", "W".');
@@ -214,4 +219,84 @@ describe('Input to 2 rovers and their output', () => {
   });
 });
 
+describe('createPlateau', () => {
+  it('should create a Plateau object with the correct width and depth', () => {
+    const result = createPlateau('5 5');
+    expect(result).toEqual({ width: 6, depth: 6 });
+  });
 
+  it('should return an error message if the input string is not in the correct format', () => {
+    const result = createPlateau('55');
+    expect(result).toEqual({ index: 1, userMessage: 'Invalid sizeString format. Expected format: "x y" where x and y are numbers.' });
+  });
+});
+
+describe('createRover', () => {
+  it('should create a Rover object with the correct x, y, and direction values', () => {
+    const result = createRover('1 2 N');
+    expect(result).toEqual({ x: 1, y: 2, direction: 'N' });
+  });
+
+  it('should return an error message if the input string is not in the correct format', () => {
+    const result = createRover('12N');
+    expect(result).toEqual({ index: 2, userMessage: 'Invalid roverString format. Expected format: "x y d" where x and y are numbers and d is one of "N", "E", "S", "W".' });
+  });
+});
+
+describe('isRoverCollidingWithPoints', () => {
+  it('should return true if the rover collides with any of the points in the array', () => {
+    const rover = { x: 1, y: 2, direction: 'N' };
+    const points = [{ posX: 1, posY: 2 }];
+    const result = isRoverCollidingWithPoints(rover, points);
+    expect(result).toBe(true);
+  });
+
+  it('should return false if the rover does not collide with any of the points in the array', () => {
+    const rover = { x: 1, y: 2, direction: 'N' };
+    const points = [{ posX: 3, posY: 4 }];
+    const result = isRoverCollidingWithPoints(rover, points);
+    expect(result).toBe(false);
+  });
+});
+
+describe('move', () => {
+  it('should move the rover one unit forward in its current direction if the command is "M"', () => {
+    const rover = { x: 1, y: 2, direction: 'N' };
+    const plateau = { width: 6, depth: 6 };
+    const result = move(rover, 'M', plateau);
+    expect(result).toEqual({ x: 1, y: 3, direction: 'N' });
+  });
+
+  it('should return an error message if the new position is out of bounds', () => {
+    const rover = { x: 5, y: 5, direction: 'N' };
+    const plateau = { width: 6, depth: 6 };
+    const result = move(rover, 'M', plateau);
+    expect(result).toEqual({ index:3 , userMessage:'Rover@(5,5) not moved since out of bounds of plateau.' });
+  });
+});
+
+describe('isRoverCollidingWithPoints', () => {
+  it('should return true if the rover collides with any of the points in the array', () => {
+    const rover = { x: 1, y: 2, direction: 'N' };
+    const points = [{ posX: 1, posY: 2 }];
+    const result = isRoverCollidingWithPoints(rover, points);
+    expect(result).toBe(true);
+  });
+
+  it('should return false if the rover does not collide with any of the points in the array', () => {
+    const rover = { x: 1, y: 2, direction: 'N' };
+    const points = [{ posX: 3, posY: 4 }];
+    const result = isRoverCollidingWithPoints(rover, points);
+    expect(result).toBe(false);
+  });
+});
+
+describe('instructionsRover', () => {
+  it('should return an error message if a collision is detected between the rover and any of the collision points', () => {
+    const rover = { x: 1, y: 2, direction: 'N' };
+    const plateau = { width: 6, depth: 6 };
+    const points = [{ posX: 1, posY: 3 }];
+    const result = instructionsRover(rover, plateau, 'M', points);
+    expect(result).toEqual({ index: 4, userMessage: 'Collision detected between rover@(1,3) and collision point.' });
+  });
+});
