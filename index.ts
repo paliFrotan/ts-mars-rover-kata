e30fcb13a864ac7fe5de7ea14eb6f8f61a730b36
Pali Frotan <pali.frotan@icloud.com>
2023-06-16 21:48:26 +0100

    Fix: amended index file for 2 errors

--- index.ts
+++ index.ts
@@ -11,7 +11,8 @@ import * as readline from 'readline';
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
+    terminal: false
 });
 
 
@@ -24,6 +25,8 @@ rl.question('Enter plateau size: ', (answer) => {
     let ans = createPlateau(answer);
     if ('index' in ans) {
         console.log(ans.userMessage);
+        rl.close();
+        process.exit();
     }
     function addRover() {
         rl.question('Enter rover starting position and direction: ', (roverInput) => {
@@ -31,18 +34,20 @@ rl.question('Enter plateau size: ', (answer) => {
             if ('index' in rover) {
                 console.log(rover.userMessage);
                 rl.close();
+                process.exit();
             }
             else {
                 rl.question('Enter instructions: ', (instructions) => {
                     if ('x' in rover  && 'width' in ans){
                         let newPosition = instructionsRover(rover, ans, instructions,collisionPoints);
-                        console.log(newPosition);
+                        console.log("Rover moved to: ", newPosition);
                     }
                     rl.question('Add another rover? (y/n): ', (addAnother) => {
                         if (addAnother.toLowerCase() === 'y') {
                             addRover();
                         } else {
                             rl.close();
+                            process.exit();
                         }
                     });
                 });
