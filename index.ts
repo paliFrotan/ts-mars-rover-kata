cc01994e51d4db509cfcbefce40ba5dc548edb0f
Pali Frotan <pali.frotan@icloud.com>
2023-06-17 13:45:54 +0100

    Fix: heading two colours

--- index.ts
+++ index.ts
@@ -15,55 +15,64 @@ const rl = readline.createInterface({
     output: process.stdout,
     terminal: false
 });
+ 
 
-function reset() {
-    console.log("System crashed ....");
+function reset(ans: ErrorMessage, newValue: any) {
+    console.log("System failure ....");
+    ans.userMessage = newValue;
+    //let rerun = 'n';
     rl.question("Do you want to rerun?", (rerun)=> {
+        
         if (rerun.toLowerCase() === 'y') 
            start();
         else {
            console.clear();
-           process.exit();
+           //process.exit();
         };
     }) 
-
 }
+
 
 function start() {
     console.clear();
-    console.log("----------------------------------------------------------");
-    console.log("|                 Mars Rover Mission                     |")
-    console.log("----------------------------------------------------------");
+    console.log("\x1b[32m%s\x1b[0m", "----------------------------------------------------------");
+    console.log("\x1b[32m%s\x1b[0m" + "                Mars Rover Mission                    " + "\x1b[32m%s\x1b[0m", "|*", "*|");
+
+    console.log("\x1b[32m%s\x1b[0m", "----------------------------------------------------------");
     let collisionPoints: CollisionPoints[] = [{ posX: -1, posY: -1}];
     rl.question('Enter plateau size: ', (answer) => {
         let ans = createPlateau(answer);
         if ('index' in ans) {
             console.log(ans.userMessage);
-            rl.close();
+            reset(ans, '');
             // process.exit();
         }
-        function addRover(ans: Plateau) {
+        function addRover(ans: Plateau | ErrorMessage) {
             rl.question('Enter rover starting position and direction: ', (roverInput) => {
-                let rover = createRover(roverInput, ans);
-                if ('index' in rover) {
-                    console.log(rover.userMessage);
-                    reset();
-                    //process.exit();
-                }
-                else {
-                    rl.question('Enter instructions: ', (instructions) => {
-                        if ('x' in rover && 'width' in ans) {
-                            let newPosition = instructionsRover(rover, ans, instructions, collisionPoints);
-                            console.log("Rover moved to: ", newPosition);
-                        }
-                        rl.question('Add another rover? (y/n): ', (addAnother) => {
-                            if (addAnother.toLowerCase() === 'y') {
-                                addRover(ans);
-                            } else {
-                                start();
+                if (!('index' in ans)){
+                    let rover = createRover(roverInput, ans);
+                
+                    if ('index' in rover) {
+                        console.log(rover.userMessage);
+                        reset(rover, '');
+                        //process.exit();
+                    }
+                
+                    else {
+                        rl.question('Enter instructions: ', (instructions) => {
+                            if ('x' in rover && 'width' in ans) {
+                                let newPosition = instructionsRover(rover, ans, instructions, collisionPoints);
+                                console.log("Rover moved to: ", newPosition);
                             }
+                            rl.question('Add another rover? (y/n): ', (addAnother) => {
+                                if (addAnother.toLowerCase() === 'y') {
+                                    addRover(ans);
+                                } else {
+                                    start();
+                                }
+                            });
                         });
-                    });
+                    };
                 };
             });
         }
